6ae74a3c3b77036ca6cc2ba244b32b339abae5d5
Phillip Ferentinos <phillipjf@users.noreply.github.com>
2022-02-11 11:26:27 -0600

    refactor(unused_variables): refactor unused_variables module for simplification and to not be a script

--- tf_utils/utils.py
+++ tf_utils/utils.py
@@ -1,7 +1,17 @@
 import os
 
+import hcl2 as hcl
 
-def find_tf_modules(root="."):
+
+def load_terraform(path):
+    try:
+        with open(path) as tf:
+            return hcl.load(tf)
+    except ValueError as err:
+        raise ValueError(f"Error loading Terraform from {path}: {err}")
+
+
+def module_file_list(root="."):
     ignored_directories = ["/terraform.tfstate.d", "/.terraform", "/.git"]
     modules = {}
     for mod_root, _, filenames in os.walk(root):
